c08be9cfa444fcc97acb7ceb728a09367352fdb9
Teletha <Teletha.T@gmail.com>
2012-11-30 15:34:42 +0900

    Refactoring NodeDebugger.

--- src/test/java/booton/translator/UnsafeWordCalculator.java
+++ src/test/java/booton/translator/UnsafeWordCalculator.java
@@ -1,17 +1,11 @@
 /*
- * Copyright (C) 2009 Nameless Production Committee.
+ * Copyright (C) 2012 Nameless Production Committee
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *          http://opensource.org/licenses/mit-license.php
  */
 package booton.translator;
 
@@ -21,7 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * @version 2009/09/01 17:33:05
+ * @version 2012/11/30 15:33:24
  */
 public class UnsafeWordCalculator {
 
