cbd4885a421599efd924949569e6ff745283c3c3
Teletha <Teletha.T@gmail.com>
2012-12-07 08:43:24 +0900

    Progress...

--- src/main/java/teemowork/model/Patch.java
+++ src/main/java/teemowork/model/Patch.java
@@ -9,16 +9,31 @@
  */
 package teemowork.model;
 
+import booton.translator.js.JsArray;
+
 /**
  * @version 2012/12/06 23:07:37
  */
 public class Patch {
 
-    public static Patch P1520 = new Patch(1510, 2012, 11, 13, "Preseason 3");
+    /** The patch. */
+    public static Patch P1510 = new Patch(1510, 2012, 11, 13, "End of Season 2", null);
+
+    static {
+        P1510.update(Item.HauntingGuise).mrpen(20);
+    }
+
+    /** The patch. */
+    public static Patch P1520 = new Patch(1520, 2012, 12, 03, "Preseason 3", P1510);
 
     static {
         P1520.update(Item.ShardOfTrueIce);
+        P1520.update(Item.LiandrysTorment);
+        P1520.update(Item.HauntingGuise).mrpen(15);
     }
+
+    /** The latest patch. */
+    public static Patch latest = P1520;
 
     /** The update number. */
     public final int number;
@@ -26,12 +41,19 @@ public class Patch {
     /** The update name. */
     public final String name;
 
+    /** The previous {@link Patch}. */
+    private final Patch previous;
+
+    /** The item list. */
+    private final JsArray<Item> items = new JsArray();
+
     /**
      * Create patch information.
      */
-    public Patch(int number, int year, int month, int day, String name) {
+    public Patch(int number, int year, int month, int day, String name, Patch previous) {
         this.number = number;
         this.name = name;
+        this.previous = previous;
     }
 
     /**
@@ -41,8 +63,9 @@ public class Patch {
      * 
      * @param item
      */
-    public Item update(Item item) {
-        return null;
+    private Item update(Item item) {
+        items.push(item);
+        return item;
     }
 
     /**
@@ -50,5 +73,12 @@ public class Patch {
      */
     private static class ItemUpdate {
 
+    }
+
+    /**
+     * @return
+     */
+    public JsArray<Item> getItems() {
+        return items;
     }
 }
