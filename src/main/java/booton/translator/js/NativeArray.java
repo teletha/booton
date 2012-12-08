826596d0e27b27b46d9d537e369d56d87830666c
Teletha <Teletha.T@gmail.com>
2012-12-08 11:33:03 +0900

    Implement js native Object, Array and Map.

---
+++ src/main/java/booton/translator/js/NativeArray.java
@@ -0,0 +1,285 @@
+/*
+ * Copyright (C) 2012 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package booton.translator.js;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import booton.translator.Translator;
+
+/**
+ * @version 2012/12/08 11:08:56
+ */
+public class NativeArray<T> extends NativeObject implements Iterable<T> {
+
+    /** The java emulation. */
+    private final ArrayList<T> list = new ArrayList();
+
+    /**
+     * <p>
+     * Retrieve the item at the specified index.
+     * </p>
+     * 
+     * @param index A array index;
+     * @return A item at index.
+     */
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    /**
+     * <p>
+     * Set the item at the specified index.
+     * </p>
+     * 
+     * @param index A array index;
+     * @param value A value to set.
+     * @return A value to set.
+     */
+    public T set(int index, T value) {
+        list.ensureCapacity(index);
+
+        return list.set(index, value);
+    }
+
+    /**
+     * <p>
+     * Reflects the number of elements in an array.
+     * </p>
+     * 
+     * @return A array size.
+     */
+    public int length() {
+        return list.size();
+    }
+
+    /**
+     * <p>
+     * Adds one or more elements to the end of an array and returns the new length of the array.
+     * </p>
+     * 
+     * @param item The elements to add to the end of the array.
+     * @return The new length property of the object upon which the method was called.
+     */
+    public int push(T item) {
+        list.add(item);
+
+        return list.size();
+    }
+
+    /**
+     * <p>
+     * The pop method removes the last element from an array and returns that value to the caller.
+     * </p>
+     * 
+     * @return A removed item.
+     */
+    public T pop() {
+        return list.isEmpty() ? null : list.remove(list.size() - 1);
+    }
+
+    /**
+     * <p>
+     * Removes the first element from an array and returns that element. This method changes the
+     * length of the array.
+     * </p>
+     * 
+     * @return A removed item.
+     */
+    public T shift() {
+        return list.isEmpty() ? null : list.remove(0);
+    }
+
+    /**
+     * <p>
+     * Adds one or more elements to the front of an array and returns the new length of the array.
+     * </p>
+     * 
+     * @return The new length property of the object upon which the method was called.
+     */
+    public int unshift(T item) {
+        list.add(0, item);
+
+        return list.size();
+    }
+
+    /**
+     * <p>
+     * Returns the first (least) index of an element within the array equal to the specified value,
+     * or -1 if none is found.
+     * </p>
+     * 
+     * @param item A search item.
+     * @return A first index or -1 if none is found.
+     */
+    public int indexOf(Object item) {
+        return list.indexOf(item);
+    }
+
+    /**
+     * <p>
+     * Returns the last (greatest) index of an element within the array equal to the specified
+     * value, or -1 if none is found.
+     * </p>
+     * 
+     * @param item A search item.
+     * @return A last index or -1 if none is found.
+     */
+    public int lastIndexOf(Object item) {
+        return list.lastIndexOf(item);
+    }
+
+    /**
+     * <p>
+     * Changes the content of an array, adding new elements while removing old elements.
+     * </p>
+     */
+    public NativeArray<T> remove(int index, int length) {
+        // If this exception will be thrown, it is bug of this program. So we must rethrow the
+        // wrapped error in here.
+        throw new Error();
+    }
+
+    /**
+     * <p>
+     * Changes the content of an array, adding new elements while removing old elements.
+     * </p>
+     */
+    public void splice(int index, int length, NativeArray<T> items) {
+        // If this exception will be thrown, it is bug of this program. So we must rethrow the
+        // wrapped error in here.
+        throw new Error();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    /**
+     * @version 2012/12/08 11:09:31
+     */
+    @SuppressWarnings("unused")
+    private static class Coder extends Translator<NativeArray> {
+
+        /**
+         * <p>
+         * Create array.
+         * </p>
+         * 
+         * @return
+         */
+        public String NativeArray() {
+            return "[]";
+        }
+
+        /**
+         * <p>
+         * Retrieve the item at the specified index.
+         * </p>
+         * 
+         * @param index A array index;
+         * @return A item at index.
+         */
+        public String get(int index) {
+            return that + "[" + param(0) + "]";
+        }
+
+        /**
+         * <p>
+         * Set the item at the specified index.
+         * </p>
+         * 
+         * @param index A array index;
+         * @param value A value to set.
+         * @return A value to set.
+         */
+        public String set(int index, Object value) {
+            return that + "[" + param(0) + "]=" + param(1);
+        }
+
+        /**
+         * <p>
+         * Reflects the number of elements in an array.
+         * </p>
+         * 
+         * @return A array size.
+         */
+        public String length() {
+            return that + ".length";
+        }
+
+        /**
+         * <p>
+         * Adds one or more elements to the end of an array and returns the new length of the array.
+         * </p>
+         * 
+         * @param item The elements to add to the end of the array.
+         * @return The new length property of the object upon which the method was called.
+         */
+        public String push(Object param0) {
+            return that + ".push(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Returns the first (least) index of an element within the array equal to the specified
+         * value, or -1 if none is found.
+         * </p>
+         * 
+         * @param item A search item.
+         * @return A first index or -1 if none is found.
+         */
+        public String indexOf(Object item) {
+            return that + ".indexOf(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Returns the last (greatest) index of an element within the array equal to the specified
+         * value, or -1 if none is found.
+         * </p>
+         * 
+         * @param item A search item.
+         * @return A last index or -1 if none is found.
+         */
+        public String lastIndexOf(Object item) {
+            return that + ".lastIndexOf(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Changes the content of an array, adding new elements while removing old elements.
+         * </p>
+         */
+        public String remove(int param0, int param1) {
+            return that + ".splice(" + param(0) + "," + param(1) + ")";
+        }
+
+        /**
+         * <p>
+         * Changes the content of an array, adding new elements while removing old elements.
+         * </p>
+         */
+        public String splice(int param0, int param1, NativeArray param2) {
+            return that + ".splice(" + param(0) + "," + param(1) + "," + param(2) + ")";
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public String iterator() {
+            return that + ".it()";
+        }
+    }
+}
--- src/main/java/booton/translator/js/NativeMap.java
+++ src/main/java/booton/translator/js/NativeMap.java
@@ -9,13 +9,160 @@
  */
 package booton.translator.js;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import booton.translator.Translator;
 
 /**
- * @version 2012/12/08 2:25:12
+ * <p>
+ * Javascript native Map implementation in Java.
+ * </p>
+ * 
+ * @version 2012/12/08 9:52:59
  */
-public class NativeMap<K, V> {
+public class NativeMap<K, V> extends NativeObject {
 
-    public native V set(K key, V value);
+    /** The java emulation. */
+    private final Map<K, V> container = new HashMap();
 
-    public native V get(Object key);
+    /**
+     * <p>
+     * Returns the value associated to the key, or undefined if there is none.
+     * </p>
+     * 
+     * @param key
+     * @return
+     */
+    public V get(Object key) {
+        return container.get(key);
+    }
+
+    /**
+     * <p>
+     * Sets the value for the key in myMap. Returns undefined.
+     * </p>
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    public V set(K key, V value) {
+        return container.put(key, value);
+    }
+
+    /**
+     * <p>
+     * Returns a boolean asserting whether a value has been associated to the key in myMap or not
+     * </p>
+     * 
+     * @param key
+     * @return
+     */
+    public boolean has(Object key) {
+        return container.containsKey(key);
+    }
+
+    /**
+     * <p>
+     * Removes any value associated to the key. After such a call, myMap.has(key) will return false.
+     * </p>
+     * 
+     * @param key
+     * @return
+     */
+    public V delete(Object key) {
+        return container.remove(key);
+    }
+
+    /**
+     * <p>
+     * Returns the number of key/value pairs in {@link NativeMap}.
+     * </p>
+     * 
+     * @return
+     */
+    public int size() {
+        return container.size();
+    }
+
+    /**
+     * @version 2012/12/08 9:56:53
+     */
+    @SuppressWarnings("unused")
+    private static class Coder extends Translator<NativeMap> {
+
+        /**
+         * <p>
+         * Create Map.
+         * </p>
+         * 
+         * @return
+         */
+        public String NativeMap() {
+            return "Map()";
+        }
+
+        /**
+         * <p>
+         * Returns the value associated to the key, or undefined if there is none.
+         * </p>
+         * 
+         * @param key
+         * @return
+         */
+        public String get(Object key) {
+            return that + ".get(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Sets the value for the key in myMap. Returns undefined.
+         * </p>
+         * 
+         * @param key
+         * @param value
+         * @return
+         */
+        public String set(Object key, Object value) {
+            return that + ".set(" + param(0) + "," + param(1) + ")";
+        }
+
+        /**
+         * <p>
+         * Returns a boolean asserting whether a value has been associated to the key in myMap or
+         * not
+         * </p>
+         * 
+         * @param key
+         * @return
+         */
+        public String has(Object key) {
+            return that + "has(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Removes any value associated to the key. After such a call, myMap.has(key) will return
+         * false.
+         * </p>
+         * 
+         * @param key
+         * @return
+         */
+        public String delete(Object key) {
+            return that + "delete(" + param(0) + ")";
+        }
+
+        /**
+         * <p>
+         * Returns the number of key/value pairs in {@link NativeMap}.
+         * </p>
+         * 
+         * @return
+         */
+        public String size() {
+            return that + ".size()";
+        }
+    }
 }
