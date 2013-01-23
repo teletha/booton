1f3480c245468623501e16cc2f7351375ed9f75c
Teletha <Teletha.T@gmail.com>
2013-01-24 08:43:38 +0900

    Progress...

---
+++ src/main/java/teemowork/model/ImprovementEntry.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package teemowork.model;
+
+/**
+ * @version 2013/01/23 23:37:52
+ */
+public enum ImprovementEntry {
+
+    AS,
+
+    ASPerLv,
+
+    AD,
+
+    ADPerLv,
+
+    Critical,
+
+    CriticalPerLv,
+
+    LS,
+
+    LSPerLv,
+
+    Health,
+
+    HealthPerLv,
+
+    Hreg,
+
+    HregPerLv,
+
+    Mana,
+
+    ManaPerLv,
+
+    Mreg,
+
+    MregPerLv,
+
+    AP,
+
+    APPerLv,
+
+    SV,
+
+    SVPerLv,
+
+    CDR,
+
+    CDRPerLv,
+
+    MS,
+
+    MSPercentage,
+
+    AR,
+
+    ARPerLv,
+
+    MR,
+
+    MRPerLv,
+
+    ARPen,
+
+    ARPenPercentage,
+
+    MRPen,
+
+    MRPenPercentage;
+}
