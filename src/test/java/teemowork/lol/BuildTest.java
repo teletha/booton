17c988c84904fd979545bb715564569f987b0299
Teletha <Teletha.T@gmail.com>
2013-01-30 13:51:05 +0900

    Item is class now.

--- src/test/java/teemowork/lol/BuildTest.java
+++ src/test/java/teemowork/lol/BuildTest.java
@@ -9,7 +9,11 @@
  */
 package teemowork.lol;
 
+import static teemowork.lol.Status.*;
+
 import org.junit.Test;
+
+import teemowork.lol.Build.Computed;
 
 /**
  * @version 2013/01/30 12:46:23
@@ -17,7 +21,49 @@ import org.junit.Test;
 public class BuildTest {
 
     @Test
-    public void build() throws Exception {
-        Build build = new Build(Champion.Ahri);
+    public void instance() throws Exception {
+        Champion champion = new EmptyChampion();
+        Build build = new Build(champion);
+        assert build.champion == champion;
+        assert build.get(AD).base == 0;
+    }
+
+    @Test
+    public void chmpion() throws Exception {
+        Champion champion = new EmptyChampion();
+        champion.update(Version.P0000).set(AD, 20);
+
+        Build build = new Build(champion);
+        Computed valeu = build.get(AD);
+        assert valeu.base == 20;
+        assert valeu.increased == 0;
+        assert valeu.value == 20;
+    }
+
+    @Test
+    public void item() throws Exception {
+        Champion champion = new EmptyChampion();
+
+        Build build = new Build(champion);
+
+        Computed valeu = build.get(AD);
+        assert valeu.base == 20;
+        assert valeu.increased == 0;
+        assert valeu.value == 20;
+    }
+
+    /**
+     * @version 2013/01/30 13:34:11
+     */
+    private static class EmptyChampion extends Champion {
+
+        /**
+         * 
+         */
+        private EmptyChampion() {
+            super("Tester", null, null, null, null, null);
+
+            update(Version.P0000);
+        }
     }
 }
