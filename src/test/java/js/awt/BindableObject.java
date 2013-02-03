87e2833f3faa87b8795dd564c67cab27407b79e1
Teletha <Teletha.T@gmail.com>
2013-02-03 17:39:13 +0900

    Progress...

---
+++ src/test/java/js/awt/BindableObject.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.awt;
+
+/**
+ * @version 2013/02/03 15:25:14
+ */
+class BindableObject {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "BindableObject";
+    }
+}
---
+++ src/test/java/js/awt/Text.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.awt;
+
+
+/**
+ * @version 2013/02/03 14:57:09
+ */
+public class Text extends Widget {
+
+}
