b37dec117d992d200107e07202295c9f853c9e8e
Teletha <Teletha.T@gmail.com>
2013-02-15 16:36:07 +0900

    Add new page.

---
+++ src/main/java/js/ui/Table.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.ui;
+
+import java.util.Collection;
+
+import js.util.jQuery;
+
+/**
+ * @version 2013/02/15 15:27:46
+ */
+public abstract class Table<T> extends UI {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void compose(jQuery parent) {
+    }
+
+    /**
+     * <p>
+     * Set image sources.
+     * </p>
+     * 
+     * @return
+     */
+    protected abstract Collection<T> sources();
+}
