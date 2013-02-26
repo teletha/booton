5e8a45492de64af95c049e22a52a0f141c2c5f98
Teletha <Teletha.T@gmail.com>
2013-02-26 17:08:09 +0900

    FIX: CD is not aware of CDR.

--- src/main/java/teemowork/ChampionDetail.java
+++ src/main/java/teemowork/ChampionDetail.java
@@ -339,26 +339,38 @@ public class ChampionDetail extends Page {
             }
         }
 
-        private void write(jQuery root, SkillStatus status, Variable variable) {
+        /**
+         * <p>
+         * Write skill related status.
+         * </p>
+         * 
+         * @param root A element to write.
+         * @param skill A current processing skill.
+         * @param variable A target skill variable.
+         */
+        private void write(jQuery root, SkillStatus skill, Variable variable) {
             root.empty();
 
             if (variable != null) {
-                VariableResolver resolver = variable.getResolver();
-                String label = variable.getStatus().name;
+                Status status = variable.getStatus();
 
-                if (root == cost && status.getType() == SkillType.Toggle) {
+                // write label
+                String label = status.name;
+
+                if (status != Range && status != CD && skill.getType() == SkillType.Toggle) {
                     label = "毎秒" + label;
                 }
                 root.child(ValueStyles.Label.class).text(label);
 
-                int size = resolver.estimateSize();
+                // write values
+                int size = variable.getResolver().estimateSize();
 
                 for (int i = 1; i <= size; i++) {
-                    double value = variable.getStatus().round(variable.calculate(i, build));
+                    double value = status.round(variable.calculate(i, build));
                     jQuery element = root.child(ValueStyles.Value.class).text(value == -1 ? "∞" : value);
 
                     if (size != 1 && i == build.getLevel(this.skill)) {
-                        element.addClass(ValueStyles.Current.class);
+                        element.addClass(SkillStyle.Current.class);
                     }
 
                     if (i != size) {
@@ -366,17 +378,11 @@ public class ChampionDetail extends Page {
                     }
                 }
 
-                if (!variable.amplifiers.isEmpty()) {
-                    root.append("(");
+                // write amplifiers
+                writeAmplifier(root, variable.amplifiers, 0);
 
-                    for (Variable amplifier : variable.amplifiers) {
-                        writeAmplifier(root, amplifier, 0);
-                    }
-
-                    root.append(")");
-                }
-
-                root.append(variable.getStatus().getUnit());
+                // write unit
+                root.append(status.getUnit());
             }
         }
 
@@ -384,20 +390,19 @@ public class ChampionDetail extends Page {
          * @param root
          * @param variable
          * @param size
-         * @param skillLevel
+         * @param level
          */
-        private void writeVariable(jQuery root, Variable variable, int skillLevel) {
+        private void writeVariable(jQuery root, Variable variable, int level) {
             VariableResolver resolver = variable.getResolver();
             Status status = variable.getStatus();
             List<Variable> amplifiers = variable.amplifiers;
 
             if (!resolver.isSkillLevelBased()) {
-                skillLevel = resolver.convertLevel(build.getLevel());
+                level = resolver.convertLevel(build.getLevel());
             }
 
             // compute current value
-            root.child(SkillStyle.Computed.class)
-                    .text(status.format(variable.calculate(Math.max(1, skillLevel), build)));
+            root.child(SkillStyle.Computed.class).text(status.format(variable.calculate(Math.max(1, level), build)));
 
             // All values
             int size = resolver.estimateSize();
@@ -408,7 +413,7 @@ public class ChampionDetail extends Page {
                 for (int i = 1; i <= size; i++) {
                     jQuery element = root.child(SkillStyle.Value.class).text(resolver.compute(i));
 
-                    if (i == skillLevel) {
+                    if (i == level) {
                         element.addClass(SkillStyle.Current.class);
                     }
 
@@ -417,51 +422,54 @@ public class ChampionDetail extends Page {
                     }
                 }
 
-                if (!amplifiers.isEmpty()) {
-                    for (Variable amplifier : amplifiers) {
-                        writeAmplifier(root, amplifier, skillLevel);
-                    }
-                }
+                writeAmplifier(root, amplifiers, level);
                 root.append(")");
             }
         }
 
-        private void writeAmplifier(jQuery root, Variable amplifier, int skillLevel) {
-            jQuery element = root.child(SkillStyle.Amplifier.class);
-            element.append("+");
+        /**
+         * <p>
+         * Write skill amplifier.
+         * </p>
+         * 
+         * @param root A element to write.
+         * @param amplifiers A list of skill amplifiers.
+         * @param level A current skill level.
+         */
+        private void writeAmplifier(jQuery root, List<Variable> amplifiers, int level) {
+            for (Variable amplifier : amplifiers) {
+                jQuery element = root.child(SkillStyle.Amplifier.class);
+                element.append("+");
 
-            VariableResolver resolver = amplifier.getResolver();
+                VariableResolver resolver = amplifier.getResolver();
 
-            if (!resolver.isSkillLevelBased()) {
-                skillLevel = resolver.convertLevel(build.getLevel());
-            }
-
-            int size = resolver.estimateSize();
-
-            for (int i = 1; i <= size; i++) {
-                jQuery value = element.child(SkillStyle.Value.class)
-                        .text(Mathematics.round(amplifier.calculate(i, build), 3));
-
-                if (size != 1 && i == skillLevel) {
-                    value.addClass(SkillStyle.Current.class);
+                if (!resolver.isSkillLevelBased()) {
+                    level = resolver.convertLevel(build.getLevel());
                 }
 
-                if (i != size) {
-                    element.child(SkillStyle.Separator.class).text("/");
+                int size = resolver.estimateSize();
+
+                for (int i = 1; i <= size; i++) {
+                    jQuery value = element.child(SkillStyle.Value.class)
+                            .text(Mathematics.round(amplifier.calculate(i, build), 3));
+
+                    if (size != 1 && i == level) {
+                        value.addClass(SkillStyle.Current.class);
+                    }
+
+                    if (i != size) {
+                        element.child(SkillStyle.Separator.class).text("/");
+                    }
                 }
-            }
 
-            element.append(amplifier.getStatus().getUnit());
-
-            if (!amplifier.amplifiers.isEmpty()) {
-                element.append("(");
-                for (Variable nest : amplifier.amplifiers) {
-                    writeAmplifier(element, nest, skillLevel);
+                element.append(amplifier.getStatus().getUnit());
+                if (!amplifier.amplifiers.isEmpty()) {
+                    element.append("(");
+                    writeAmplifier(element, amplifier.amplifiers, level);
+                    element.append(")");
                 }
-                element.append(")");
+                element.append(amplifier.getStatus().name);
             }
-
-            element.append(amplifier.getStatus().name);
         }
     }
 
@@ -512,17 +520,6 @@ public class ChampionDetail extends Page {
             {
                 box.opacity(0.4);
                 margin.horizontal(4, px);
-            }
-        }
-
-        /**
-         * @version 2013/02/06 20:03:25
-         */
-        private static class Current extends CSS {
-
-            {
-                font.color(rgba(160, 123, 1, 1));
-                box.opacity(1);
             }
         }
     }
