8a9cd5696a06b806ffac76383bee2ee37c98eb15
Teletha <Teletha.T@gmail.com>
2013-04-06 13:19:40 +0900

    Debuggable annotation makes debug easy.

---
+++ src/test/java/booton/translator/Debuggable.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package booton.translator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @version 2013/04/06 13:14:00
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface Debuggable {
+}
