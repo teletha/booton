ac44147f241ca0d7ca36326f4e82104dffb7a3f7
Teletha <Teletha.T@gmail.com>
2013-04-11 11:07:54 +0900

    FIX: ScriptTester doesn't re-use scriptable instance for each tests.

--- src/test/java/booton/translator/operator/InstanceOfTest.java
+++ src/test/java/booton/translator/operator/InstanceOfTest.java
@@ -1,17 +1,11 @@
 /*
- * Copyright (C) 2009 Nameless Production Committee.
+ * Copyright (C) 2013 Nameless Production Committee
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *          http://opensource.org/licenses/mit-license.php
  */
 package booton.translator.operator;
 
@@ -23,7 +17,7 @@ import booton.translator.ScriptTester;
 import booton.translator.Scriptable;
 
 /**
- * @version 2012/12/01 3:43:44
+ * @version 2013/04/11 11:05:08
  */
 @SuppressWarnings("unused")
 public class InstanceOfTest extends ScriptTester {
