95178d7af022fc7be4eec4340c4a97d855dc3ec1
Teletha <Teletha.T@gmail.com>
2013-06-10 08:31:27 +0900

    Progress.....

---
+++ src/main/java/teemowork/ItemViewStyle.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package teemowork;
+
+import booton.css.CSS;
+
+/**
+ * @version 2013/06/09 19:05:20
+ */
+public class ItemViewStyle {
+
+    /**
+     * @version 2013/06/09 22:49:26
+     */
+    class Root extends CSS {
+
+        {
+            display.table();
+        }
+    }
+}
