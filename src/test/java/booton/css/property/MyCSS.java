35b2d9cc97ab63b90bfa4358959565ea2c6c8f32
Teletha <Teletha.T@gmail.com>
2013-07-16 16:59:41 +0900

    CSS is testable.

---
+++ src/test/java/booton/css/property/MyCSS.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package booton.css.property;
+
+import kiss.I;
+import booton.Booton;
+import booton.css.CSS;
+
+/**
+ * @version 2013/07/16 16:10:32
+ */
+public class MyCSS extends CSS {
+
+    static {
+        I.load(Booton.class, false);
+    }
+
+    /**
+     * <p>
+     * Test property name and value.
+     * </p>
+     * 
+     * @param name
+     * @param value
+     * @return
+     */
+    boolean has(String name, String value) {
+        return toString().contains(name + ":" + value + ";");
+    }
+}
