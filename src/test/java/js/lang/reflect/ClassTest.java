499b3dc9b3a16bc409027cb3d364d9a866cdcc4b
Teletha <Teletha.T@gmail.com>
2013-08-03 03:31:25 +0900

    Object.class#getSuperClass will returns null.

--- src/test/java/js/lang/reflect/ClassTest.java
+++ src/test/java/js/lang/reflect/ClassTest.java
@@ -10,6 +10,8 @@
 package js.lang.reflect;
 
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 
 import org.junit.Test;
 
@@ -23,14 +25,55 @@ import booton.translator.Scriptable;
 public class ClassTest extends ScriptTester {
 
     @Test
-    public void getName() throws Exception {
+    public void modifierPrivate() throws Exception {
         test(new Scriptable() {
 
             int act() {
                 int modifier = Private.class.getModifiers();
                 assert Modifier.isPrivate(modifier);
 
-                return Private.class.getModifiers();
+                return modifier;
+            }
+        });
+    }
+
+    @Test
+    public void modifierPackage() throws Exception {
+        test(new Scriptable() {
+
+            int act() {
+                int modifier = Package.class.getModifiers();
+                assert !Modifier.isPrivate(modifier);
+                assert !Modifier.isProtected(modifier);
+                assert !Modifier.isPublic(modifier);
+
+                return modifier;
+            }
+        });
+    }
+
+    @Test
+    public void modifierProtected() throws Exception {
+        test(new Scriptable() {
+
+            int act() {
+                int modifier = Protected.class.getModifiers();
+                assert Modifier.isProtected(modifier);
+
+                return modifier;
+            }
+        });
+    }
+
+    @Test
+    public void modifierPublic() throws Exception {
+        test(new Scriptable() {
+
+            int act() {
+                int modifier = Public.class.getModifiers();
+                assert Modifier.isPublic(modifier);
+
+                return modifier;
             }
         });
     }
@@ -39,5 +82,91 @@ public class ClassTest extends ScriptTester {
      * @version 2013/08/03 0:25:14
      */
     private static class Private {
+    }
+
+    /**
+     * @version 2013/08/03 0:25:14
+     */
+    static class Package {
+    }
+
+    /**
+     * @version 2013/08/03 0:25:14
+     */
+    protected static class Protected {
+    }
+
+    /**
+     * @version 2013/08/03 0:25:14
+     */
+    public static class Public {
+    }
+
+    @Test
+    public void superClass1() throws Exception {
+        test(new Scriptable() {
+
+            Class act() {
+                return Child.class.getSuperclass();
+            }
+        });
+    }
+
+    @Test
+    public void superClass2() throws Exception {
+        test(new Scriptable() {
+
+            Class act() {
+                return Parent.class.getSuperclass();
+            }
+        });
+    }
+
+    @Test
+    public void superClass3() throws Exception {
+        test(new Scriptable() {
+
+            Class act() {
+                return Object.class.getSuperclass();
+            }
+        });
+    }
+
+    /**
+     * @version 2013/08/03 2:49:26
+     */
+    private static class Parent {
+    }
+
+    /**
+     * @version 2013/08/03 2:49:45
+     */
+    private static class Child extends Parent {
+    }
+
+    @Test
+    public void genericSuperClass1() throws Exception {
+        test(new Scriptable() {
+
+            int act() {
+                Type type = TypeVariableChild.class.getGenericSuperclass();
+                assert type != null;
+                assert type instanceof TypeVariable;
+
+                return 1;
+            }
+        });
+    }
+
+    /**
+     * @version 2013/08/03 2:49:26
+     */
+    private static class TypeVariableParent<T> {
+    }
+
+    /**
+     * @version 2013/08/03 2:49:45
+     */
+    private static class TypeVariableChild extends TypeVariableParent<Parent> {
     }
 }
