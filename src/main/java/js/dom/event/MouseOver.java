d8e44b8985d3b30692e9482ab5f629f6c2ee8fd1
Teletha <Teletha.T@gmail.com>
2013-10-20 08:32:03 +0900

    Progress....

---
+++ src/main/java/js/dom/event/MouseOver.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2013 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.dom.event;
+
+/**
+ * @version 2013/10/19 9:30:39
+ */
+public class MouseOver extends DOMEvent {
+}
