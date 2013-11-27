52751bbd8ac6fc0bf5add7c7350b6417c9a2e235
Teletha <Teletha.T@gmail.com>
2013-11-27 15:59:32 +0900

    Clean up code.

--- src/main/java/booton/translator/JavaMethodCompiler.java
+++ src/main/java/booton/translator/JavaMethodCompiler.java
@@ -260,9 +260,9 @@ class JavaMethodCompiler extends MethodVisitor {
         }
         debugger.whileProcess = true;
 
-        if (script.source.getName().endsWith("ChampionComparing$2") && original.equals("<init>")) {
-            debugger.enable = true;
-        }
+        // if (script.source.getName().endsWith("String") && original.equals("regionMatches")) {
+        // debugger.enable = true;
+        // }
     }
 
     /**
