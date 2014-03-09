5df1baf11cdacfc33f157f7a61ff7ab47b94a594
Teletha <Teletha.T@gmail.com>
2014-03-10 08:33:43 +0900

    Progress...

---
+++ src/test/java/js/lang/NativeDateTimeFormatTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.lang;
+
+import java.time.LocalDate;
+
+import js.lang.NativeIntl.DateTimeFormat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import booton.soeur.ScriptRunner;
+
+/**
+ * @version 2014/03/09 23:02:23
+ */
+@RunWith(ScriptRunner.class)
+public class NativeDateTimeFormatTest {
+
+    @Test
+    public void format() throws Exception {
+
+        DateTimeFormat format = new DateTimeFormat("en");
+        String text = format.format(LocalDate.of(2014, 3, 7));
+        System.out.println(text);
+    }
+}
