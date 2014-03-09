d741a5be5e66cc8e7e4f9dce1b6e78564196aca7
Teletha <Teletha.T@gmail.com>
2014-03-10 08:33:43 +0900

    Progress...

---
+++ src/main/java/js/lang/NativeIntl.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package js.lang;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
+
+import booton.translator.Translator;
+
+/**
+ * @version 2014/03/09 20:41:43
+ */
+public class NativeIntl {
+
+    /**
+     * @version 2014/03/09 22:53:15
+     */
+    public static class DateTimeFormat {
+
+        /** The formatter. */
+        private final DateTimeFormatter format;
+
+        /**
+         * <p>
+         * The Intl.DateTimeFormat object is a constructor for objects that enable language
+         * sensitive date and time formatting.
+         * </p>
+         */
+        public DateTimeFormat(String locales) {
+            format = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT);
+        }
+
+        /**
+         * <p>
+         * Returns a getter function that formats a date according to the locale and formatting
+         * options of this DateTimeFormat object.
+         * </p>
+         * 
+         * @param date The date to format.
+         * @return
+         */
+        public String format(TemporalAccessor date) {
+            return format.format(date);
+        }
+
+        /**
+         * @version 2014/03/09 22:56:14
+         */
+        private static class Coder extends Translator<DateTimeFormat> {
+
+        }
+    }
+
+    /**
+     * @version 2014/03/09 20:42:39
+     */
+    public static class NumberFormat {
+
+        /**
+         * <p>
+         * The Intl.NumberFormat object is a constructor for objects that enable language sensitive
+         * number formatting.
+         * </p>
+         * 
+         * @param locales
+         */
+        public NumberFormat(String locales) {
+
+        }
+
+        /**
+         * @version 2014/03/09 21:00:44
+         */
+        private static class Coder extends Translator<NumberFormat> {
+
+        }
+    }
+}
