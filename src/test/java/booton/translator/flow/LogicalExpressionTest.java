5d04da7a74def5dbc793aa24a479423c4d1d39e5
Teletha <Teletha.T@gmail.com>
2014-06-19 09:40:42 +0900

    Restrict ternary operator creation.

--- src/test/java/booton/translator/flow/LogicalExpressionTest.java
+++ src/test/java/booton/translator/flow/LogicalExpressionTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import booton.soeur.Param;
 import booton.soeur.ScriptTester;
 import booton.soeur.Scriptable;
+import booton.translator.Debuggable;
 
 /**
  * @version 2013/11/04 17:19:06
@@ -675,6 +676,7 @@ public class LogicalExpressionTest extends ScriptTester {
     public void ConditionalOperatorNest3() {
         test(new Scriptable() {
 
+            @Debuggable
             public int act(@Param(from = 0, to = 5) int value) {
                 return value % 2 == 0 ? value == 2 || value == 4 ? 20 : 10 : value;
             }
