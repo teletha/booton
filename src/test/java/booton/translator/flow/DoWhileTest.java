90a630db2c9dc0dac3c5d27107cd96dab1c2f2b8
Teletha <Teletha.T@gmail.com>
2014-07-07 02:41:05 +0900

    FIX: Booton can't compile labeled inifinite loop.

--- src/test/java/booton/translator/flow/DoWhileTest.java
+++ src/test/java/booton/translator/flow/DoWhileTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import booton.soeur.Param;
 import booton.soeur.ScriptTester;
 import booton.soeur.Scriptable;
-import booton.translator.Debuggable;
 
 /**
  * @version 2014/01/16 22:36:52
@@ -281,7 +280,6 @@ public class DoWhileTest extends ScriptTester {
     public void nestBreakJump() {
         test(new Scriptable() {
 
-            @Debuggable
             public int act(int value) {
                 root: do {
                     value++;
