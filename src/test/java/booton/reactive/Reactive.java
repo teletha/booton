cb14bf515783c566de452acbc6947b062517177a
Teletha <Teletha.T@gmail.com>
2014-08-21 17:05:54 +0900

    What is reactive?

---
+++ src/test/java/booton/reactive/Reactive.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package booton.reactive;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+import booton.reactive.css.Font;
+
+/**
+ * @version 2014/08/21 13:31:25
+ */
+public abstract class Reactive {
+
+    protected void hbox(Runnable child) {
+
+    }
+
+    protected void text(Object... texts) {
+        if (texts == null) {
+            return;
+        }
+
+        for (Object text : texts) {
+
+        }
+    }
+
+    protected abstract void template();
+
+    /**
+     * @version 2014/08/21 13:36:52
+     */
+    public static class Variable<T> extends SimpleObjectProperty<T> {
+
+        protected final Font font = new Font();
+
+        /**
+         * Define style.
+         */
+        protected void style() {
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            return String.valueOf(get());
+        }
+    }
+}
