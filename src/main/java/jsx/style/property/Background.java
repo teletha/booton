ba4df80b2f6d336639670d9fcbecb73b73907649
Teletha <Teletha.T@gmail.com>
2014-10-27 16:36:20 +0900

    Progress...

---
+++ src/main/java/jsx/style/property/Background.java
@@ -0,0 +1,413 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.style.property;
+
+import booton.css.Unit;
+import booton.css.value.Color;
+import booton.css.value.LinearGradient;
+
+/**
+ * @version 2014/10/27 16:28:21
+ */
+public class Background extends Colorable<Background> {
+
+    private String verticalPosition;
+
+    private String horizontalPosition;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Color color() {
+        Color backgroundColor = color;
+
+        if (backgroundColor == null) {
+            for (Object image : images) {
+                if (image instanceof LinearGradient) {
+                    backgroundColor = ((LinearGradient) image).getEndColor();
+                    break;
+                }
+            }
+        }
+
+        if (backgroundColor == null) {
+            backgroundColor = Color.Transparent;
+        }
+        return backgroundColor;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Background color(Color color) {
+        this.color = color;
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The CSS background-image property sets the background images for an element. The images are
+     * drawn on successive stacking context layers, with the first specified being drawn as if it is
+     * the closest to the user. The borders of the element are then drawn on top of them, and the
+     * background-color is drawn beneath them.
+     * </p>
+     * 
+     * @param uri
+     * @return
+     */
+    public Background imageNone() {
+        images.clear();
+
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The CSS background-image property sets the background images for an element. The images are
+     * drawn on successive stacking context layers, with the first specified being drawn as if it is
+     * the closest to the user. The borders of the element are then drawn on top of them, and the
+     * background-color is drawn beneath them.
+     * </p>
+     * 
+     * @param uri
+     * @return
+     */
+    public Background image(String uri) {
+        images.add(url(uri));
+
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The CSS background-image property sets the background images for an element. The images are
+     * drawn on successive stacking context layers, with the first specified being drawn as if it is
+     * the closest to the user. The borders of the element are then drawn on top of them, and the
+     * background-color is drawn beneath them.
+     * </p>
+     * 
+     * @param uri
+     * @return
+     */
+    public Background image(Object... images) {
+        for (Object image : images) {
+            if (image instanceof String) {
+                this.images.add(url((String) image));
+            } else {
+                this.images.add(image);
+            }
+        }
+
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background repeatX() {
+        repeat = "repeat-x";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background repeatY() {
+        repeat = "repeat-y";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background repeat() {
+        repeat = "repeat";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background space() {
+        repeat = "space";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background round() {
+        repeat = "round";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-repeat CSS property defines how background images are repeated. A background
+     * image can be repeated along the horizontal axis, the vertical axis, both, or not repeated at
+     * all. When the repetition of the image tiles doesn't let them exactly cover the background,
+     * the way adjustments are done can be controlled by the author: by default, the last image is
+     * clipped, but the different tiles can instead be re-sized, or space can be inserted between
+     * the tiles.
+     * </p>
+     * 
+     * @return
+     */
+    public Background noRepeat() {
+        repeat = "no-repeat";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background top() {
+        verticalPosition = "top";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background bottom() {
+        verticalPosition = "bottom";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background left() {
+        horizontalPosition = "left";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background right() {
+        horizontalPosition = "right";
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background vertical(double size, Unit unit) {
+        horizontalPosition = compute(size, unit);
+
+        // Chainable API
+        return chain();
+    }
+
+    /**
+     * <p>
+     * The background-position CSS property sets the initial position, relative to the background
+     * position layer defined by background-origin for each defined background image.
+     * </p>
+     * 
+     * @return
+     */
+    public Background horizontal(double size, Unit unit) {
+        horizontalPosition = compute(size, unit);
+
+        // Chainable API
+        return value("background")
+    }
+
+    /**
+     * <p>
+     * The background-size CSS property specifies the size of the background images. The size of the
+     * image can be fully constrained or only partially in order to preserve its intrinsic ratio.
+     * </p>
+     * <p>
+     * A value that scales the background image to the specified length in the corresponding
+     * dimension. Negative lengths are not allowed.
+     * </p>
+     * 
+     * @return
+     */
+    public Background size(double size, Unit unit) {
+        return value("background-size", compute(size, unit));
+    }
+
+    /**
+     * <p>
+     * The background-size CSS property specifies the size of the background images. The size of the
+     * image can be fully constrained or only partially in order to preserve its intrinsic ratio.
+     * </p>
+     * <p>
+     * This keyword specifies that the background image should be scaled to be as small as possible
+     * while ensuring both its dimensions are greater than or equal to the corresponding dimensions
+     * of the background positioning area.
+     * </p>
+     * 
+     * @return
+     */
+    public Background cover() {
+        return value("background-size", "cover");
+    }
+
+    /**
+     * <p>
+     * The background-size CSS property specifies the size of the background images. The size of the
+     * image can be fully constrained or only partially in order to preserve its intrinsic ratio.
+     * </p>
+     * <p>
+     * This keyword specifies that the background image should be scaled to be as large as possible
+     * while ensuring both its dimensions are less than or equal to the corresponding dimensions of
+     * the background positioning area.
+     * </p>
+     * 
+     * @return
+     */
+    public Background contain() {
+        return value("background-size", "contain");
+    }
+
+    /**
+     * <p>
+     * The background-origin CSS property determines the background positioning area, that is the
+     * position of the origin of an image specified using the background-image CSS property.
+     * </p>
+     * <p>
+     * No background is drawn below the border (background extends to the outside edge of the
+     * padding).
+     * </p>
+     * 
+     * @return
+     */
+    public Background paddingBox() {
+        return value("background-origin", "padding-box");
+    }
+
+    /**
+     * <p>
+     * The background-origin CSS property determines the background positioning area, that is the
+     * position of the origin of an image specified using the background-image CSS property.
+     * </p>
+     * <p>
+     * The background is painted within (clipped to) the content box.
+     * </p>
+     * 
+     * @return
+     */
+    public Background contentBox() {
+        return value("background-origin", "content-box");
+    }
+
+    /**
+     * <p>
+     * The background-origin CSS property determines the background positioning area, that is the
+     * position of the origin of an image specified using the background-image CSS property.
+     * </p>
+     * <p>
+     * The background extends to the outside edge of the border (but underneath the border in
+     * z-ordering).
+     * </p>
+     * 
+     * @return
+     */
+    public Background borderBox() {
+        return value("background-origin", "border-box");
+    }
+}
