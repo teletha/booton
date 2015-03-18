a75a7abe6e31832afe08ff98c47903c5176cd463
Teletha <Teletha.T@gmail.com>
2015-03-18 15:35:14 +0900

    Progress...

--- src/main/java/jsx/ui/piece/StyleProperty.java
+++ src/main/java/jsx/ui/piece/StyleProperty.java
@@ -9,12 +9,22 @@
  */
 package jsx.ui.piece;
 
+import js.lang.NativeArray;
+import jsx.collection.DualList;
 import jsx.style.Style;
+import kiss.Disposable;
+import kiss.Events;
 
 /**
  * @version 2015/03/06 15:31:03
  */
-public class StyleProperty {
+public class StyleProperty implements Style, Disposable {
+
+    /** The managed style list. */
+    private DualList<Style, Boolean> styles = new DualList();
+
+    /** The disposer. */
+    private Disposable disposer = Disposable.Φ;
 
     /**
      * @param shown
@@ -22,4 +32,46 @@ public class StyleProperty {
     public StyleProperty(Style style) {
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void declare() {
+        // do nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void assignTo(NativeArray<Style> styles, DualList<String, String> inlines) {
+        for (int i = 0, size = this.styles.size(); i < size; i++) {
+            if (this.styles.value(i)) {
+                this.styles.key(i).assignTo(styles, inlines);
+            }
+        }
+    }
+
+    public void toggle() {
+        NativeArray<Boolean> values = styles.values();
+
+        for (int i = 0, size = values.length(); i < size; i++) {
+            values.set(i, !values.getAsBoolean(i));
+        }
+    }
+
+    public StyleProperty withIf(Events<Boolean> condition, Style additional) {
+        disposer = disposer.and(condition.to(v -> styles.set(additional, v)));
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dispose() {
+        styles.clear();
+        disposer.dispose();
+    }
 }
