e7d2935a27da4c20f117261fadca87c3eab5b7ac
Teletha <Teletha.T@gmail.com>
2015-03-22 17:32:12 +0900

    Progress...

--- src/main/java/jsx/ui/piece/CheckBox.java
+++ src/main/java/jsx/ui/piece/CheckBox.java
@@ -10,6 +10,7 @@
 package jsx.ui.piece;
 
 import static js.dom.UIAction.*;
+import static jsx.ui.piece.FormStyle.*;
 
 import javafx.beans.property.BooleanProperty;
 
@@ -38,6 +39,17 @@ public class CheckBox extends LowLevelWidget<CheckBox> {
      */
     @Override
     protected void virtualize(VirtualStructure 〡) {
+        〡.nbox.〡(CheckBoxRoot, () -> {
+            〡.e("input", "type", "checkbox");
+            〡.e("label").〡(() -> {
+                〡.nbox.〡(null, "text");
+            });
+            〡.e("s:svg", CheckBoxSVG, "viewBox", "0 0 100 100")
+                    .〡(() -> {
+                        〡.e("s:path", "d", "M 10 10 L 90 90", "style", "stroke: #FDFCD3; stroke-width: 13px; stroke-linecap: round; stroke-linejoin: round; fill: none; stroke-dasharray: 113.137, 113.137; stroke-dashoffset: 0; transition: stroke-dashoffset 0.2s ease-in-out 0s;");
+                    });
+
+        });
         〡.e("input", 0).〡(() -> {
             〡.attr.〡("type", "checkbox");
             〡.attr.〡("checked", check.get() ? "checked" : "");
---
+++ src/main/java/jsx/ui/piece/FormStyle.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.ui.piece;
+
+import jsx.style.Style;
+import jsx.style.StyleRuleDescriptor;
+
+/**
+ * @version 2015/03/22 16:46:20
+ */
+public class FormStyle extends StyleRuleDescriptor {
+
+    static Style CheckBoxRoot = () -> {
+        position.relative();
+    };
+
+    static Style CheckBoxSVG = () -> {
+        position.absolute().top(50, percent).left(5, px);
+        box.size(40, px);
+
+    };
+}
