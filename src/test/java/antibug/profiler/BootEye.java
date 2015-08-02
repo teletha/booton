c77d2eb65982253f2563dcf943aebde7bf9feca3
Teletha <Teletha.T@gmail.com>
2015-08-02 15:45:07 +0900

    Progress...

---
+++ src/test/java/antibug/profiler/BootEye.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.profiler;
+
+/**
+ * @version 2015/08/02 9:10:31
+ */
+public enum BootEye implements ProfileGroup<Class> {
+
+    RunTest;
+}
