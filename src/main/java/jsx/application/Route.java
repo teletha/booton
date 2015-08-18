8359c6f855546209dbe335d70e1e1e8cff32fec2
Teletha <Teletha.T@gmail.com>
2015-08-19 08:50:48 +0900

    Progress...

---
+++ src/main/java/jsx/application/Route.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.application;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @version 2015/08/18 10:37:26
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface Route {
+}
