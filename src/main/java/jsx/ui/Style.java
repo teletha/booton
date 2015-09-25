f66eb3026319b967196e7634516aa8cd1bdd21ee
Teletha <Teletha.T@gmail.com>
2015-09-25 09:02:22 +0900

    Style is top-level class.

---
+++ src/main/java/jsx/ui/Style.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.ui;
+
+import js.dom.UIEvent;
+import jsx.style.StyleName;
+
+/**
+ * @version 2015/09/21 16:12:55
+ */
+public interface Style extends Declarable, Locatable<UIEvent> {
+
+    /**
+     * <p>
+     * Define the style declaration.
+     * </p>
+     */
+    void style();
+
+    /**
+     * <p>
+     * Compute style class name.
+     * </p>
+     * 
+     * @return A style class name.
+     * @deprecated This method is internal API. Use {@link StyleName#of(Style)} instead.
+     */
+    @Deprecated
+    public default String className() {
+        return "STYLE" + hashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    default void declare() {
+        StructureDescriptor.latestElement.classList.push(this);
+    }
+}
