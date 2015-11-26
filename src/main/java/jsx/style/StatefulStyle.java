a4fd87eb739ccc2b540b7266af9a1b71bdf1ccf7
Teletha <Teletha.T@gmail.com>
2015-11-26 16:28:53 +0900

    Add StatefulStyle.

---
+++ src/main/java/jsx/style/StatefulStyle.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.style;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+import jsx.ui.Declarable;
+import jsx.ui.Location;
+
+/**
+ * @version 2015/11/26 10:45:29
+ */
+public class StatefulStyle<T> extends SimpleObjectProperty<T>implements Location<T>, Declarable {
+
+    private final ValueStyle<T> style;
+
+    /**
+     * @param style
+     */
+    private StatefulStyle(ValueStyle<T> style) {
+        this.style = style;
+    }
+
+    public static <T> StatefulStyle<T> of(ValueStyle<T> style) {
+        return new StatefulStyle(style);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void declare() {
+        style.style(get());
+    }
+}
