13d36e0f2bc31b80e5f2fa9173a57a0a1398ba77
Teletha <Teletha.T@gmail.com>
2016-09-25 14:17:15 +0900

    Provide ViewDSL.

---
+++ src/main/java/jsx/ui/ViewDSL.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package jsx.ui;
+
+/**
+ * @version 2016/09/25 14:07:21
+ */
+public abstract class ViewDSL extends StructureDSL {
+
+    protected abstract void virtualize();
+}
