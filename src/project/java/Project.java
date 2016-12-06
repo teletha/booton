77fee1d0535713f1b8e73aba910d324bdad33f7e
Teletha <Teletha.T@gmail.com>
2016-12-06 16:24:39 +0900

    Revert Jetty.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -10,18 +10,17 @@
 public class Project extends bee.api.Project {
 
     {
-        String jettyVersion = "9.4.0.RC2";
+        String jettyVersion = "9.2.19.v20160908";
 
         product("npc", "Booton", "0.1");
         producer("Nameless Production Committee");
 
         require("npc", "sinobu", "1.0");
-        require("net.sourceforge.htmlunit", "htmlunit", "2.23").atTest();
         require("org.eclipse.jetty.websocket", "websocket-server", jettyVersion);
         require("org.eclipse.jetty", "jetty-servlet", jettyVersion);
         require("org.eclipse.jetty", "jetty-util", jettyVersion);
         require("org.eclipse.jetty", "jetty-http", jettyVersion);
         require("npc", "antibug", "0.3").atTest();
-
+        require("net.sourceforge.htmlunit", "htmlunit", "2.23").atTest();
     }
 }
