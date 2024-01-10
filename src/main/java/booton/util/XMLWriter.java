a416724e962baf5e9fb048398d69a6aed9611344
Teletha <Teletha.T@gmail.com>
2024-01-10 09:29:52 +0900

    progress

--- src/main/java/booton/util/XMLWriter.java
+++ src/main/java/booton/util/XMLWriter.java
@@ -23,8 +23,6 @@ import org.xml.sax.Attributes;
 import org.xml.sax.ext.LexicalHandler;
 import org.xml.sax.helpers.XMLFilterImpl;
 
-import com.sun.org.apache.xml.internal.serializer.TreeWalker;
-
 import kiss.I;
 import kiss.XML;
 
@@ -543,13 +541,11 @@ public class XMLWriter extends XMLFilterImpl implements LexicalHandler {
     public void write(XML xml) {
         try {
             startDocument();
-            TreeWalker walker = new TreeWalker(this);
 
             Field nodes = XML.class.getDeclaredField("nodes");
             nodes.setAccessible(true);
 
             for (Node node : (List<Node>) nodes.get(xml)) {
-                walker.traverse(node);
             }
             endDocument();
         } catch (Exception e) {
